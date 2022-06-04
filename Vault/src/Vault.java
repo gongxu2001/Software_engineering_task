bbe3dd5a5f80ba6e339139695163956725650435
gongxu <2524186573@qq.com>
2022-06-04 20:52:25 +0800

    跳马代码优化

--- Vault/src/Vault.java
+++ Vault/src/Vault.java
@@ -14,27 +14,52 @@ public class Vault {
     static String path = "";
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         System.out.print("请输入棋盘行数:");
-        vault_row = sc.nextInt();
+        vault_row = ScannerInt();
         System.out.print("请输入棋盘列数:");
-        vault_col = sc.nextInt();
+        vault_col = ScannerInt();
 
-        System.out.print("请输入起点行坐标:");
-        start_row = sc.nextInt();
-        System.out.print("请输入起点行坐标:");
-        start_col = sc.nextInt();
+        while (true) {
+            System.out.print("请输入起点行坐标:");
+            start_row = ScannerInt();
 
-        System.out.print("请输入终点行坐标:");
-        stop_row = sc.nextInt();
-        System.out.print("请输入终点列坐标:");
-        stop_col = sc.nextInt();
+            System.out.print("请输入起点列坐标:");
+            start_col = ScannerInt();
+            if (start_row < 0 || start_col < 0 || start_row > vault_row || start_col > vault_col) {
+                System.out.println("越界了请重新输入");
+                continue;
+            }
+            break;
+        }
 
+        while (true) {
+            System.out.print("请输入终点行坐标:");
+            stop_row = ScannerInt();
+
+            System.out.print("请输入终点列坐标:");
+            stop_col = ScannerInt();
+            if (stop_row < start_row) {
+                System.out.println("好马不走回头路,终点位置有错，请重新输入");
+                continue;
+            } else if (stop_row > vault_row || stop_col > vault_col) {
+                System.out.println("越界了请重新输入");
+                continue;
+            }
+            break;
+        }
 
         dfs(start_row, start_col);
         System.out.println("总路线有:" + count + "条");
     }
 
+    public static int ScannerInt() {
+        Scanner sc = new Scanner(System.in);
+        while (!(sc.hasNextInt())) {
+            System.out.print("您输入的数量不是整型，请重新输入:");
+            sc.next();
+        }
+        return sc.nextInt();
+    }
 
     public static void dfs(int row, int col) {
         path += "(" + row + "," + col + ")";
